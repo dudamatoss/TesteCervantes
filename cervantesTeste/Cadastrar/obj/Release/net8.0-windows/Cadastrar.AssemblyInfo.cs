b9d16be3ee821a4a949a15eb09ba1b121bc0ab13
dudamatos <dudasantosmatos15@gmail.com>
2025-04-22 12:18:55 -0300

    ajuste final

--- cervantesTeste/Cadastrar/obj/Release/net8.0-windows/Cadastrar.AssemblyInfo.cs
+++ cervantesTeste/Cadastrar/obj/Release/net8.0-windows/Cadastrar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cadastrar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6be7b615bf92d1c35810752a8922495de8a9d99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c855188a112aa8e0486e19a210959aad221e1a5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cadastrar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cadastrar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
